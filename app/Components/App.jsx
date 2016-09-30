23477dfc8d867fb0bfb6f71ca0b2302fae07d4c5
p-herbert <pete.a.herbert@gmail.com>
2016-09-30 14:54:09 -0700

    (feat) Create skeleton for App.jsx

---
+++ app/Components/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <p>Hello world!</p>
+    );
+  }
+}
+
+export default App;
+
