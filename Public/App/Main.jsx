3d01a4faa7f15b5d364fd59a281f754bd4da33d0
p-herbert <pete.a.herbert@gmail.com>
2016-09-30 19:16:04 -0700

    (feat) Create Main.jsx

---
+++ Public/App/Main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './Components/App';
+
+// Top level React App file
+render(<App />, document.getElementById('app'));
+
