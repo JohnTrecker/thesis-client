21edb46fe082fe179f38690fa7b83c4f58f5c6d6
p-herbert <pete.a.herbert@gmail.com>
2016-09-30 20:07:19 -0700

    (feat) Create Search component

---
+++ Public/App/Components/Search.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Row, Input } from 'react-materialize';
+
+const Search = () => (
+  <Row>
+    <Input label="Search" center />
+  </Row>
+);
+
+export default Search;
