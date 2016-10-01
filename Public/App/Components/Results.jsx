3f1b75dce34e41cc360c92377a3e6a49b1e4183a
p-herbert <pete.a.herbert@gmail.com>
2016-10-01 10:08:53 -0700

    (refactor) Import Post component

--- Public/App/Components/Results.jsx
+++ Public/App/Components/Results.jsx
@@ -1,24 +1,10 @@
 import React from 'react';
 import { Row, Col, Collection, CollectionItem } from 'react-materialize';
+import Post from './Post';
 
 const Results = () => (
   <Collection>
-    <CollectionItem>
-      <Row>
-        <Col className="left-align" s={6}>Title: Blog Post</Col>
-        <Col className="right-align" s={6}>Ranking: 1</Col>
-      </Row>
-      <Row>
-        <Col className="left-align">Description: Technical Blog Post</Col>
-      </Row>
-      <Row>
-        <Col className="left-align" s={6}>Author: Mike Smith</Col>
-        <Col className="right-align" s={6}>Date: Today</Col>
-      </Row>
-      <Row>
-        <Col className="left-align">Tags: Tag1 Tag2 Tag3</Col>
-      </Row>
-    </CollectionItem>
+    <Post />
   </Collection>
 );
 
