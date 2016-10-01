d12daddaf060ec9785417659f3a17a9c22f5431d
p-herbert <pete.a.herbert@gmail.com>
2016-10-01 09:52:55 -0700

    (refactor) Make Entry component stateful

--- Public/App/Components/Entry.jsx
+++ Public/App/Components/Entry.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Row, Col, Collection, CollectionItem } from 'react-materialize';
 import Link from './Link';
 
@@ -6,28 +6,44 @@ const author = "ICANN";
 const title = "Example Domain";
 const link = "www.example.com";
 
-const Entry = () => (
-  <Row>
-    <Collection>
-      <CollectionItem>
-        <Row>
-          <Col className="left-align" s={6}>Title: Blog Post</Col>
-          <Col className="right-align" s={6}>Ranking: 1</Col>
-        </Row>
-        <Row>
-          <Col className="left-align" s={10}>
-            <p>
-              Description: Technical Blog Post blah blah blah blah blah blah blah blah
-              blah blah blah blah blah blah blah
-            </p>
-          </Col>
-        </Row>
-      </CollectionItem>
-    </Collection>
-    <Collection>
-      <Link link={link} title={title} author={author} />
-    </Collection>
-  </Row>
-);
+class Entry extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      entry: this.props.entry,
+      links: this.props.links };
+  }
+
+  render() {
+    return (
+      <Row>
+        <Collection>
+          <CollectionItem>
+            <Row>
+              <Col className="left-align" s={6}>Title: Blog Post</Col>
+              <Col className="right-align" s={6}>Ranking: 1</Col>
+            </Row>
+            <Row>
+              <Col className="left-align" s={10}>
+                <p>
+                  Description: Technical Blog Post blah blah blah blah blah blah blah blah
+                  blah blah blah blah blah blah blah
+                </p>
+              </Col>
+            </Row>
+          </CollectionItem>
+        </Collection>
+        <Collection>
+          <Link link={link} title={title} author={author} />
+        </Collection>
+      </Row>
+    );
+  }
+}
+
+Entry.propTypes = {
+  entry: React.PropTypes.object,
+  links: React.PropTypes.array };
 
 export default Entry;
