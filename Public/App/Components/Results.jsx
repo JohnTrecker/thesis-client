90e5f68d18efa4302cb8d5d43b3ea81b2d986d06
p-herbert <pete.a.herbert@gmail.com>
2016-10-01 10:46:40 -0700

    (refactor) Make Results component stateful

--- Public/App/Components/Results.jsx
+++ Public/App/Components/Results.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Row, Col, Collection, CollectionItem } from 'react-materialize';
 import Post from './Post';
 
-const Results = () => (
-  <Collection>
-    <Post />
-  </Collection>
-);
+class Results extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: this.prop.posts };
+  }
+
+  render() {
+    return (
+      <Collection>
+        <Post title={"Blog Post"} rank={1} desc={"Technical Blog Post"} author={"Mike Smith"} date={"Today"} tags={["Tag1 ", "Tag2 ", "Tag3 "]} />
+      </Collection>
+    );
+  }
+}
+
+Results.propTypes = {
+  posts: React.PropTypes.array };
 
 export default Results;
